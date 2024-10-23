dd3da96099b708831d066fbf3de5d6ae47088b52
luan0811 <luanntmse173311@fpt.edu.vn>
2024-10-23 16:07:37 +0700

    lam lai trang home

--- FE/src/services/flower.ts
+++ FE/src/services/flower.ts
@@ -3,7 +3,10 @@ import apiClient from "./api"
 const getFlowers = async (page: number, limit: number, search: string) => {
     const response = await apiClient.get(`/flower/get-list?page=${page}&limit=${limit}&search=${search}`)
     console.log('response', response.data)
-    return response.data
+    return {
+        flowers: response.data,
+        hasMore: response.data.length === limit
+    }
 }
 
 const getFlowerById = async (id: string) => {
