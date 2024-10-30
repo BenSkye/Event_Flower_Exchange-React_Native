c26e72da7b4d6103ca6509da72613e3fd48565d5
gkhanh07 <gkhanh0706@gmail.com>
2024-10-30 23:36:13 +0700

    1

--- FE/src/screens/OrderDetail.tsx
+++ FE/src/screens/OrderDetail.tsx
@@ -3,6 +3,10 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import React, { useLayoutEffect, useEffect, useState } from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
 import { getOrderbyOrdercode } from '../services/order';
+import { Button, Text, Image } from 'react-native-elements';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { ORDER_STATUS_LABELS } from '../constant/indext';
+import { formatPrice } from '../utils';
 
 const OrderDetail = ({ route }: { route: any }) => {
     const { orderCode, pageBack } = route.params;
@@ -43,26 +47,26 @@ const OrderDetail = ({ route }: { route: any }) => {
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.header}>Order Details</Text>
             {orderDetails ? (
-                <View style={styles.detailsContainer}>
-                    <Text style={styles.label}>Order Code:</Text>
+                <View style={styles.detailContainer}>
+                    <Text style={styles.title}>Thông tin đơn hàng</Text>
+                    <Text style={styles.label}>Mã đơn hàng:</Text>
                     <Text style={styles.value}>{orderDetails.orderCode}</Text>
-                    <Text style={styles.label}>Price:</Text>
-                    <Text style={styles.value}>{orderDetails.price}</Text>
-                    <Text style={styles.label}>Status:</Text>
-                    <Text style={styles.value}>{orderDetails.status}</Text>
-                    <View style={styles.flowerContainer}>
-                        <Text style={styles.subHeader}>Flower Details</Text>
-                        <Text style={styles.label}>Name:</Text>
-                        <Text style={styles.value}>{orderDetails.flowerId.name}</Text>
-                        <Text style={styles.label}>Description:</Text>
-                        <Text style={styles.value}>{orderDetails.flowerId.description}</Text>
-                        <Image
-                            source={{ uri: orderDetails.flowerId.images[0] }}
-                            style={styles.flowerImage}
-                        />
-                    </View>
+                    <Text style={styles.label}>Giá:</Text>
+                    <Text style={styles.value}>{orderDetails.price} vnđ</Text>
+                    <Text style={styles.label}>Trạng thái đơn hàng:</Text>
+                    <Text style={styles.value}>
+                        {orderDetails.status === 'completed' ? 'Đã thanh toán' : orderDetails.status}
+                    </Text>
+                    <Text style={styles.label}>Tên:</Text>
+                    <Text style={styles.value}>{orderDetails.flowerId.name}</Text>
+                    <Text style={styles.label}>Mô tả:</Text>
+                    <Text style={styles.value}>{orderDetails.flowerId.description}</Text>
+                    <Image
+                        source={{ uri: orderDetails.flowerId.images[0] }}
+                        style={styles.flowerImage}
+                        PlaceholderContent={<Text>Loading...</Text>}
+                    />
                 </View>
             ) : (
                 <Text style={styles.loadingText}>Loading...</Text>
@@ -74,25 +78,24 @@ const OrderDetail = ({ route }: { route: any }) => {
 const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
-        padding: 20,
         backgroundColor: '#f8f9fa',
     },
-    header: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        textAlign: 'center',
-        color: '#333',
-    },
-    detailsContainer: {
+    detailContainer: {
+        padding: 16,
         backgroundColor: '#fff',
-        borderRadius: 10,
-        padding: 20,
+        borderRadius: 8,
+        margin: 16,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
-        shadowRadius: 5,
-        elevation: 3,
+        shadowRadius: 8,
+        elevation: 4,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 16,
+        textAlign: 'center',
     },
     sectionHeader: {
         flexDirection: 'row',
@@ -127,15 +130,10 @@ const styles = StyleSheet.create({
         color: '#333',
         flex: 2,
     },
-    subHeader: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 20,
-        marginBottom: 10,
-        color: '#333',
-    },
-    flowerContainer: {
-        marginTop: 20,
+    valueHighlight: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#5a61c9',
     },
     flowerImage: {
         width: '100%',
